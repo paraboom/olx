6fca92571bdd31281f1dd39443f38c2c367f38f2
Ivan Shornikov <paraboom@gmail.com>
2019-04-25 15:40:50 +0300

    Add a router

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Redirect,
+  Route,
+} from "react-router-dom";
+import Layout from "../Layout";
+
+import PageList from "../../pages/PageList";
+import PageListItem from "../../pages/PageListItem";
 
 const App = () => (
-  <div>Initial App</div>
+  <BrowserRouter>
+    <Layout>
+      <div>Hello</div>
+      <Switch>
+        <Route exact path="/list" component={PageList} />
+        <Route path="/list/aaa" component={PageListItem} />
+        <Redirect to="/list" />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
 );
 
 export default App;
